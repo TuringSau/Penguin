e505301e8e0f510f0f52c7a7511a8aebf4b8301f
saurabh patil <saurabh.p@turing.com>
2025-03-11 11:48:02 +0530

    New file added to repo

---
+++ test1.py
@@ -0,0 +1,19 @@
+a = 10
+b = 20
+
+def addition(a,b):
+	return a+b
+
+def multiplication(a,b):
+    return a*b
+    
+def division(a,b):
+    return a/b
+
+def substraction(a,b):
+    return a-b
+
+print(addition(a,b))
+print(multiplication(a,b))
+print(division(a,b))
+print(substraction(a,b))
